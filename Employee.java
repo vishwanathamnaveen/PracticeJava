fe8b430e87ef05b020c18fcbd95ad4747a25b69c
VISHWANATHAM <naveenkv@netapp.com>
2021-07-24 10:41:50 +0530

    java collections

--- Employee.java
+++ Employee.java
@@ -9,16 +9,26 @@ import java.io.*;
 //polymorphism
 
 //creating an object
+import java.util.Scanner;
 public class Employee {
     String name;
     public Employee(String name ){
         this.name=name;
        System.out.println("Employee_name"+name);
+    
+    }
+    public void  GetInputFromConsole(){
+        Scanner input =new Scanner(System.in);
+        String name=input.nextLine();
+        System.out.println("input the entry"+name);
+        int number=input.nextInt();
+        System.out.println("input the number"+number);
+    
     }
     public static void main(String args[]){
         System.out.println("creating an object");
-        Employee obj1=new Employee("Ganesh");
-        Employee obj2=new Employee("SAMEER");
+        Employee obj1=new Employee(" Ganesh");
+        Employee obj2=new Employee(" SAMEER");
 
     }
 
--- practicearrays.java
+++ practicearrays.java
@@ -1,10 +1,10 @@
-
+import java.util.ArrayList;
 public class practicearrays {
-    public static void main(String args[]){
+   /* public static void main(String args[]){
     String[] cars={ "Book", "Pencil", "Eraser", "Color", "Pen" };
     int index=0;
     boolean found=false;
-    String search="Book";
+    String search="Color";
     for(int i=0;i<cars.length;i++){
         if(search==cars[i]){
             index=i;
@@ -16,12 +16,36 @@ public class practicearrays {
         System.out.println("String found at index"+index);
     
     }else{System.out.println("String not found at index");}
+*/
 
-
-
+//printing all the elemtents in an arrary
+/*public static void main(String args[]){
+    int[] cars={ 2,3,4,5,6,7,8 };
+    for (int i=0;i<cars.length;i++){
+        System.out.println("elements in the array"+cars[i]);
+    }
 
 }
 }
 
+public static void main(String args[]){
+    int[] cars={ 2,3,4,5,600,7,8 };
+    int count=0;
+    for (int i=0;i<cars.length;i++){
+        count=count+cars[i];
+    }
+    System.out.println("count is "+count);
+
+}}*/
+//ArrayList its a dynamic a
+public static void main(String[] args){
+    ArrayList<String> students=new ArrayList<>();
+    students.add("ganesh");
+
+    System.out.println(students);
 
 
+
+}  
+
+}
